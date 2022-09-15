da1e1f4390b928f53102a5ed858ef3045baec769
traininggroupllc <phenix2017pr@gmail.com>
2022-09-15 15:08:25 +0300

    fix social data

--- apps/bridge/pages/_app.tsx
+++ apps/bridge/pages/_app.tsx
@@ -63,7 +63,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <meta
           name="description"
-          content="Cheaper and faster than Uniswap? Discover PancakeSwap, the leading DEX on BNB Smart Chain (BSC) with the best farms in DeFi and a lottery for CAKE."
+          content="BullionFX | Trade Gold ..."
         />
         <meta name="theme-color" content="#837144" />
         <meta name="twitter:image" content="https://pancakeswap.finance/images/hero.png" />
