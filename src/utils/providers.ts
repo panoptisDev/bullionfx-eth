51657793e6bc9bd250b5ec3ec4e53ab8a1b02da5
traininggroupllc <phenix2017pr@gmail.com>
2022-09-27 16:23:30 +0300

    change endpoint

--- src/utils/providers.ts
+++ src/utils/providers.ts
@@ -1,6 +1,6 @@
 import { StaticJsonRpcProvider } from '@ethersproject/providers'
 
-export const ETH_PROD_NODE = 'https://eth-mainnet.nodereal.io/v1/1659dfb40aa24bbb8153a677b98064d7'
+export const ETH_PROD_NODE = 'https://eth-mainnet.nodereal.io/v1/f84ef22f0bac4758a854933d660fb182'
 export const GOERLI_PROD_NODE = 'https://eth-goerli.nodereal.io/v1/8a4432e42df94dcca2814fde8aea2a2e'
 
 export const ethRpcProvider = new StaticJsonRpcProvider(ETH_PROD_NODE)
