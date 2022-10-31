f6db853e5390cd4d6b64dca0750dd8fa3ee1dc71
traininggroupllc <phenix2017pr@gmail.com>
2022-10-31 22:00:07 +0300

    fix credit width

--- src/views/Credit/index.tsx
+++ src/views/Credit/index.tsx
@@ -2,21 +2,19 @@ import styled from 'styled-components'
 import Container from "components/Layout/Container"
 
 const StyledIframe = styled.iframe`
-    width: 100%;
-    height: 900px;
-    @media (max-width: 1024px) {
-        height: 870px;
-    }
+    width: 500px;
+    height: 700px;
+    margin-top: 115px;
+    margin-bottom: 65px;
     @media (max-width: 768px) {
-        height: 700px;
+        margin-top: 80px;
+        margin-bottom: 30px;
     }
-    margin-top: 60px;
-    margin-bottom: 10px;
 `
 
 export default function Credit() {
     return (
-        <Container>
+        <Container style={{display: "flex"}}>
                 <StyledIframe src="https://app.kado.money?apiKey=API_KEY&amp;onPayCurrency=USD&amp;onRevCurrency=USDC&amp;offPayCurrency=USDC&amp;offRevCurrency=USD&amp;onPayAmount=20&amp;offPayAmount=20&amp;onToAddress=0x76358Ef594d630bB8eFeBfa2Fb03E97B72Be58f4&amp;cryptoList=ETH&amp;fiatList=AUD&amp;network=ETHEREUM&amp;product=BUY" title="credit" />
         </Container>
     )
